bd95573bb79785fdd099f97afc0d1225c4527aca
Amir Sarabadani <Ladsgroup@gmail.com>
2016-12-31 20:05:14 +0330

    More settings

--- tofawiki/wsgi/routes/__init__.py
+++ tofawiki/wsgi/routes/__init__.py
@@ -5,7 +5,8 @@ def configure(config, bp):
 
     @bp.route('/')
     def root():
-        return 'Hey, In order to use the service use tofawiki.wmflabs.org/translate/enwiki/Article_Name'
+        return 'Hey, In order to use the service use tofawiki.wmflabs.org/translate/enwiki/Article_Name</br>' \
+                'Source code in <a href="https://github.com/Ladsgroup/tofawiki">github</a>'
 
     bp = translate.configure(bp, config)
     return bp
